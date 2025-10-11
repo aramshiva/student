92df7194238c317fed9327bbb96468e9de942a40
Aram <me@aram.sh>
2025-10-10 22:03:40 -0700

    Update AppSidebar.tsx

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -242,7 +242,7 @@ export function AppSidebar() {
     };
     
     calculateNextPeriod();
-    const interval = setInterval(calculateNextPeriod, 12000);
+    const interval = setInterval(calculateNextPeriod, 60000);
     
     return () => clearInterval(interval);
   }, []);
