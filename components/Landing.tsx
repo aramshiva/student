68809a56f66cc1f8f9a355df826cdd6b620cc560
Aram <me@aram.sh>
2026-05-13 17:43:11 -0700

    Replace unused packages

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -14,7 +14,7 @@ import {
   useMotionValue,
   useSpring,
   useTransform,
-} from "framer-motion";
+} from "motion/react";
 
 export default function Landing() {
   const { setTheme } = useTheme();
