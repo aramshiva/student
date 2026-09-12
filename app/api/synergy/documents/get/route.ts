08cb3d936dfcc0c6caf356cd505293048c9f37c4
Aram <me@aram.sh>
2026-09-11 22:21:18 -0700

    Student is now deprecated

--- app/api/synergy/documents/get/route.ts
+++
@@ -1,8 +0,0 @@
-import { synergyRoute } from "@/lib/synergyRoute";
-
-export const runtime = "nodejs";
-
-export const POST = synergyRoute(
-  ({ client, body }) => client.getDocument(String(body.document_guid)),
-  { required: ["document_guid"] },
-);
--- components/loadingfunc.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="p-8">
-      <p>Loading gradebook...</p>
-    </div>
-  );
-}
